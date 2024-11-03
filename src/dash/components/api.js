7a5e60a259643e155a6d62a8dbaa7121b5a17384
Kali <muppal03@email.franklin.edu>
2024-11-03 07:49:09 -0500

    uncommented server URL

--- src/dash/components/api.js
+++ src/dash/components/api.js
@@ -1,1 +1,3 @@
-export const API_URL="http://localhost:3000"
+// export const API_URL="http://localhost:3000"
+// export const API_URL="https://online-restaurant-frontend.vercel.app/"
+export const API_URL="https://online-restaurant-prbp.onrender.com"
